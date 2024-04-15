ff128732579c82c55e4bbbf63bdb432198a7ef79
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro-14.local>
2024-04-15 13:14:44 -0700

    add fake

--- handlers/rest/translate_test.go
+++ handlers/rest/translate_test.go
@@ -7,8 +7,16 @@ import (
 	"testing"
 
 	"github.com/paisit04/shipping-go/handlers/rest"
-	"github.com/paisit04/shipping-go/translation"
 )
+
+type stubbedService struct{}
+
+func (s *stubbedService) Translate(word string, language string) string {
+	if word == "foo" {
+		return "bar"
+	}
+	return ""
+}
 
 func TestTranslateAPI(t *testing.T) {
 	tt := []struct {
@@ -18,26 +26,32 @@ func TestTranslateAPI(t *testing.T) {
 		ExpectedTranslation string
 	}{
 		{
-			Endpoint:            "/hello",
+			Endpoint:            "/foo",
 			StatusCode:          http.StatusOK,
 			ExpectedLanguage:    "english",
-			ExpectedTranslation: "hello",
+			ExpectedTranslation: "bar",
 		},
 		{
-			Endpoint:            "/hello?language=german",
+			Endpoint:            "/foo?language=german",
 			StatusCode:          http.StatusOK,
 			ExpectedLanguage:    "german",
-			ExpectedTranslation: "hallo",
+			ExpectedTranslation: "bar",
 		},
 		{
-			Endpoint:            "/hello?language=dutch",
+			Endpoint:            "/baz",
 			StatusCode:          http.StatusNotFound,
 			ExpectedLanguage:    "",
 			ExpectedTranslation: "",
 		},
+		// {
+		// 	Endpoint:            "/foo?language=GerMan",
+		// 	StatusCode:          http.StatusOK,
+		// 	ExpectedLanguage:    "german",
+		// 	ExpectedTranslation: "bar",
+		// },
 	}
 
-	underTest := rest.NewTranslateHandler(translation.NewStaticService())
+	underTest := rest.NewTranslateHandler(&stubbedService{})
 	handler := http.HandlerFunc(underTest.TranslateHandler)
 
 	for _, test := range tt {
